4a60a50d5db3e33868577e421a24109cf0f75e30
Markus Seeger <mail@codegourmet.de>
2014-11-12 11:28:57 +0100

    working version

--- lib/o_stream_catcher/o_stream_catcher.rb
+++ lib/o_stream_catcher/o_stream_catcher.rb
@@ -1,5 +1,39 @@
 module OStreamCatcher
 
+  def catch(&block)
+    stdout_orig, stdout_mock = mock_stdout
+    stderr_orig, stderr_mock = mock_stderr
+
+    stderr_orig = $stderr
+    stderr_mock = StringIO.new
+    $stderr = stderr_mock
+
+    begin
+      result = block.call
+    ensure
+      $stdout = stdout_orig
+      $stderr = stderr_orig
+    end
+
+    [result, stdout_mock.string, stderr_mock.string]
+  end
+
+    protected
+
+      def mock_stdout
+        orig = $stdout
+        mock = StringIO.new
+        $stdout = mock
+        [orig, mock]
+      end
+
+      def mock_stderr
+        orig = $stderr
+        mock = StringIO.new
+        $sterr = mock
+        [orig, mock]
+      end
+
   extend self
 
 end
--- test/o_stream_catcher/o_stream_catcher_test.rb
+++ test/o_stream_catcher/o_stream_catcher_test.rb
@@ -4,7 +4,28 @@ require_relative "../../lib/o_stream_catcher/o_stream_catcher"
 module OStreamCatcher
   class OStreamCatcherTest < MiniTest::Test
 
-    def test_should_be_callable
+    def test_should_return_result_of_block
+      result, _stdout, _stderr = OStreamCatcher.catch { 42 }
+      assert_equal 42, result
+    end
+
+    def test_should_return_stdout_contents
+      _result, stdout, _stderr = OStreamCatcher.catch { print "out: Hello world!" }
+      assert_equal "out: Hello world!", stdout
+    end
+
+    def test_should_return_sterr_contents
+      _result, _stdout, stderr = OStreamCatcher.catch { $stderr.print "err: Hello world!" }
+      assert_equal "err: Hello world!", stderr
+    end
+
+    def test_should_restore_streams_when_exception
+      orig_stdout = $stdout
+      orig_stderr = $stderr
+      _result = (OStreamCatcher.catch { throw "test" }) rescue []
+
+      assert_equal orig_stdout, $stdout
+      assert_equal orig_stderr, $stderr
     end
 
   end
